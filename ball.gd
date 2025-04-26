7552e055da796bb3391363f021950857d90f240b
Guang-Lin Wei <guanglinwei@gmail.com>
2025-04-26 15:40:42 -0400

    additional stuff

--- ball.gd
+++ ball.gd
@@ -1,14 +1,18 @@
 extends RigidBody2D
 
 func _ready() -> void:
+	setup();
+	
+func setup() -> void:
 	position = Vector2(800, 600);
-	pass
 	
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		print('asdjfaklsdjf')
-		self.apply_impulse(Vector2(randf() - 0.5, randf() - 0.5).normalized() * 1000);
-		self.apply_torque_impulse((randf() - 0.5) * 10000 );
+		shoot();
+		
+func shoot() -> void:
+	self.apply_impulse(Vector2(randf() - 0.5, randf() - 0.5).normalized() * 1000);
+	self.apply_torque_impulse((randf() - 0.5) * 10000 );
 	
 func _physics_process(delta: float) -> void:
 	var collision_info = move_and_collide(linear_velocity * delta);
