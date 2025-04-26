29efc3d3ccf15765e2614bd7cd7b39bc3829e6ba
Guang-Lin Wei <guanglinwei@gmail.com>
2025-04-26 12:40:22 -0400

    basic physics

---
+++ ball.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+func _ready() -> void:
+	position = Vector2(800, 600);
+	pass
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		print('asdjfaklsdjf')
+		self.apply_impulse(Vector2(randf() - 0.5, randf() - 0.5).normalized() * 1000);
+		self.apply_torque_impulse((randf() - 0.5) * 10000 );
+	
+func _physics_process(delta: float) -> void:
+	var collision_info = move_and_collide(linear_velocity * delta);
+	rotate(angular_velocity * delta);
+	if collision_info:
+		linear_velocity = linear_velocity.bounce(collision_info.get_normal());
