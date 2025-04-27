97e33f92c1869a17fc22e4272dc94de704898c05
TotallyJuicy <tj11564@gmail.com>
2025-04-27 19:33:01 -0400

    ko i added some things i think not mch

---
+++ scenes/back.gd
@@ -0,0 +1,4 @@
+extends Button
+
+func _pressed() -> void:
+	SceneManager.switch_scene('HomeScene');
--- scripts/ui/how_to_play_button.gd
+++ scripts/ui/how_to_play_button.gd
@@ -1,4 +1,4 @@
 extends Button;
 
 func _pressed() -> void:
-	print('How to play')
+	SceneManager.switch_scene('HowToPlay');
